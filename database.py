ed0fe7735d1a72d1c84ab491dcda5c8f54b8e37a
acarana <acarana@gmail.com>
2020-05-02 14:38:51 -0400

    Add files via upload

---
+++ database.py
@@ -0,0 +1,315 @@
+import psycopg2
+from tkinter import *
+from tkinter import ttk
+
+
+#Dictionary for mapping
+Patient = {
+    'ID': '',
+    'name': '',
+    'lastname': '',
+    'celphone': '',
+    'birthdate': '',
+    'address': ''
+}
+
+#DATABASE---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def select_paciente_query():
+    """
+    name = input("Nombre: ")
+    lastname = input("Apellido: ")
+    celphone = str(input('Telefono: '))
+
+    Patient['name'] = name
+    Patient['lastname'] = lastname
+    Patient['celphone'] = celphone
+    """
+    #Query to select ID of patient
+    select_id_Query = """SELECT paciente_id 
+                         FROM paciente
+                         WHERE nombre = '%s' AND apellido = '%s' AND telefono = '%s'""" %(Patient['name'], Patient['lastname'], Patient['celphone'])
+    try:
+        cursor.execute(select_id_Query)
+        ID = cursor.fetchall()
+        Patient['ID'] = ID[0]  #Add ID input to dictionary
+    except(Exception,psycopg2.Error) as error:
+         if (connection):
+            print('No se pudo encontrar ese paciente.')
+    else:
+        print_paciente_query() 
+
+
+def print_paciente_query():
+    
+    postgreSQL_select_Query = """SELECT * 
+                                 FROM paciente
+                                 WHERE nombre = '%s' AND apellido = '%s' AND telefono = '%s'""" %(Patient['name'], Patient['lastname'], Patient['celphone'])
+    cursor.execute(postgreSQL_select_Query)
+    paciente = cursor.fetchall()
+
+    print("Paciente: ")
+    for row in paciente:
+       print("paciente_id = ", row[0])
+       print("nombre = ", row[1])
+       print("apellido  = ", row[2])
+       print("fecha_nacimiento  = ", row[3])
+       print("telefono  = ", row[4])
+       print("direccion = ", row[5], "\n")
+
+
+    select_reporte_query(paciente)
+    
+
+def select_reporte_query(paciente):
+    
+    Select_Reporte_Query = """SELECT * FROM reporte WHERE reporte_id IN 
+                             (SELECT reporte_id FROM historial WHERE paciente_id = %s)""" %(Patient['ID'])
+    try:
+        cursor.execute(Select_Reporte_Query)
+        reporte = cursor.fetchall()
+    except(Exception,psycopg2.Error) as error:
+        if (connection):
+            print('No se pudo encontrar los reportes del paciente')
+    else:
+
+        print("Reporte: ")
+        for row in reporte:
+            print('reporte_id = ', row[0])
+            print('fecha_creado = ', row[1])
+            print('nota = ', row[2], '\n')
+
+        #mostrar paciente en GUI   
+        mostrar_paciente(paciente,reporte)
+
+def insert_paciente_query():
+    """
+    print('Insert Pacientes: ')
+    name = input("Nombre: ")
+    lastname = input("Apellido: ")
+    birthdate = input('Fecha de Nacimiento: ')
+    celphone = str(input('Telefono: '))
+    address = input('Direccion Fisica: ')
+
+    Patient['name'] = name
+    Patient['lastname'] = lastname
+    Patient['celphone'] = celphone
+    Patient['birthdate'] = birthdate
+    Patient['address'] = address
+    """
+    Insert_Paciente_Query = """INSERT INTO paciente(nombre,apellido,fecha_nacimiento,telefono,direccion)
+                               VALUES
+                               ('%s', '%s', '%s', '%s', '%s')""" %(Patient['name'],Patient['lastname'],Patient['birthdate'],Patient['celphone'],Patient['address'])
+    
+    try:
+        cursor.execute(Insert_Paciente_Query)
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Paciente insertado exitosamente a tabla de paciente")
+    except(Exception,psycopg2.Error) as error:
+        if (connection):
+            print('Error en insertar paciente a tabla de paciente')
+
+
+def insertar_reportes_query():
+    #query para crear reporte
+    #query para update historial
+    
+    #Para buscar reportes de persona 
+    #SELECT * FROM reporte WHERE reporte_id IN 
+    #(SELECT reporte_id FROM historial WHERE paciente_id = 3)
+    
+    pass
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#GUI----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def main_GUI_window():
+
+    root = Tk()
+    root.title('Clinica Quiropractica')
+    root.iconbitmap('Logo.ico')
+    root.geometry('400x400')
+
+    Paciente_btn = Button(root,text = 'Paciente', command=Open_Paciente).pack(fill = BOTH, expand = 1)
+    Inventario_btn = Button(root,text = 'Inventario', command=Open_Inventario).pack(fill = BOTH, expand = 1)
+
+    root.mainloop()
+
+def Open_Paciente():
+    top = Toplevel()
+    top.title('Seleccionar Paciente')
+    top.resizable(False,False)
+    top.iconbitmap('Logo.ico')
+    
+    def on_entry_click(event): 
+        if name.get() == 'Fulano':
+            name.delete(0,'end')
+        elif lastname.get() == 'Detal':
+            lastname.delete(0,'end')
+        elif celphone.get() == '1234567890':
+            celphone.delete(0,'end')
+
+        elif birthdate.get() == 'YYYY-MM-DD':
+            birthdate.delete(0,'end')
+        elif address.get() == 'Algun lugar por ahi':
+            address.delete(0,'end')
+        else:
+            return
+        
+    #text boxes
+    name = Entry(top,width=30)
+    name.grid(row=0, column=1, padx=20)
+    name.insert(0,'Fulano')
+    name.bind('<FocusIn>', on_entry_click)
+
+    lastname = Entry(top,width=30)
+    lastname.grid(row=1, column=1)
+    lastname.insert(0,'Detal')
+    lastname.bind('<FocusIn>', on_entry_click)
+
+    celphone = Entry(top,width=30)
+    celphone.grid(row=2, column=1)
+    celphone.insert(0,'1234567890')
+    celphone.bind('<FocusIn>', on_entry_click)
+
+    birthdate = Entry(top,width=30)
+    birthdate.grid(row=3,column=1)
+    birthdate.insert(0,'YYYY-MM-DD')
+    birthdate.bind('<FocusIn>', on_entry_click)
+
+    address = Entry(top,width=30)
+    address.grid(row=4,column=1)
+    address.insert(0,'Algun lugar por ahi')
+    address.bind('<FocusIn>', on_entry_click)
+
+    #text box labels
+    name_label = Label(top,text='Nombre').grid(row=0, column=0)
+    lastname_label = Label(top,text='Apellido').grid(row=1, column=0)
+    celphone_label = Label(top,text='Telefono').grid(row=2, column=0)
+    birthdate_label = Label(top,text='Fecha Nacimiento').grid(row=3, column=0)
+    address_label = Label(top,text='Direccion Fisica').grid(row=4, column=0)
+
+    #search button
+    search_btn = Button(top, text='Buscar Paciente', command=lambda: buscar_paciente(name.get(),lastname.get(),celphone.get()))
+    search_btn.grid(row=6,column=0,columnspan=2, pady=10,padx=10,ipadx=100)
+
+    #insert button
+    insert_btn = Button(top, text='Insertar Paciente', command=lambda: insertar_paciente(name.get(),lastname.get(),birthdate.get(),celphone.get(),address.get()))
+    insert_btn.grid(row=7,column=0,columnspan=2, pady=5,padx=10,ipadx=100)
+
+def buscar_paciente(name,lastname,celphone):
+    #Add input to dictionary
+    Patient['name'] = name
+    Patient['lastname'] = lastname
+    Patient['celphone'] = celphone
+    select_paciente_query()
+    print(Patient)
+
+def insertar_paciente(name,lastname,birthdate,celphone,address):
+    #Add input to dictionary
+    Patient['name'] = name
+    Patient['lastname'] = lastname
+    Patient['celphone'] = celphone
+    Patient['birthdate'] = birthdate
+    Patient['address'] = address
+    insert_paciente_query()
+    print(Patient)
+
+def mostrar_paciente(paciente,reporte):
+    top = Toplevel()
+    top.title('Paciente')
+    top.geometry('930x500')
+    top.resizable(False,False)
+    top.iconbitmap('Logo.ico')
+
+    frm = Frame(top)
+    frm.pack(side=TOP,padx=20)
+
+    #Label for patient table
+    paciente_label = Label(frm,text='Paciente')
+    paciente_label.pack(side=TOP)
+    paciente_label.config(font=("Courier", 44))
+
+    #Table por patient
+    pac = ttk.Treeview(frm,columns=(1,2,3,4,5,6),show='headings',height='2')
+    pac.pack(side=TOP,fill=X)
+    
+    pac.column(1,width=80,minwidth=50)
+    pac.column(2,width=80,minwidth=50)
+    pac.column(3,width=100,minwidth=70)
+    pac.column(4,width=120,minwidth=100)
+    pac.column(5,width=100,minwidth=70)
+    pac.column(6,width=400,minwidth=200)
+
+    pac.heading(1,text='ID',anchor=W)
+    pac.heading(2,text='Nombre',anchor=W)
+    pac.heading(3,text='Apellido',anchor=W)
+    pac.heading(4,text='Fecha Nacimiento',anchor=W)
+    pac.heading(5,text='Telefono',anchor=W)
+    pac.heading(6,text='Direccion',anchor=W)
+
+    for row in paciente:
+        pac.insert('', 'end', values=row)
+
+    #label for report table
+    reporte_label = Label(frm,text='Reportes')
+    reporte_label.pack(side=TOP)
+    reporte_label.config(font=("Courier", 44))
+
+    #Tabla for report
+    rep = ttk.Treeview(frm,columns=(1,2,3),show='headings',height='9')
+    rep.pack(side=TOP,fill=X)
+
+    rep.column(1,width=80,minwidth=50)
+    rep.column(2,width=80,minwidth=50)
+    rep.column(3,width=400,minwidth=200)
+
+    rep.heading(1,text='ID',anchor=W)
+    rep.heading(2,text='Fecha Creado',anchor=W)
+    rep.heading(3,text='Nota',anchor=W)
+
+    for row in reporte:
+        rep.insert('', 'end', values=row)
+
+    #Create Report Button
+    create_report_btn = Button(top, text='Crear Reporte', command=lambda: insertar_reportes_query)
+    create_report_btn.pack(side=TOP,pady=20,padx=10)
+
+def Open_Inventario():
+    pass
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#MAIN
+
+try:
+    connection = psycopg2.connect(user = "postgres", password = "jahesgrande01", host = "localhost", port = "5432", database = "Clinica")
+
+    cursor = connection.cursor()
+
+    # Print PostgreSQL Connection properties
+    print(connection.get_dsn_parameters(),'\n')
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+  
+    main_GUI_window()
+    #insert_paciente_query()
+    
+
+except(Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+
+finally:
+    #closing database connection
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+
+
+
+
